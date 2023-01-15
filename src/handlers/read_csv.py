fe80be9f4c7a6c2071e983f63b1aefb8637b7c03
YoungGoofy <vbi4enkov@yandex.ru>
2023-01-15 19:44:51 +0300

    read csv file

--- src/handlers/read_csv.py
+++ src/handlers/read_csv.py
@@ -1,11 +1,11 @@
 import csv
+import os.path
 
 
 def read_file():
     data = list()
     with open("test.csv", 'r') as f:
-        reader =csv.DictReader(f, delimiter=',')
+        reader = csv.DictReader(f, delimiter=',')
         for row in reader:
             data.append(row)
-        return data
-
+        return os.path.basename("test.csv"), data
