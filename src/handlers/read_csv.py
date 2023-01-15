acf10e40eea595f0019798a3e785d5b2502e2e7e
YoungGoofy <vbi4enkov@yandex.ru>
2023-01-16 01:19:10 +0300

    remove static filename

--- src/handlers/read_csv.py
+++ src/handlers/read_csv.py
@@ -2,9 +2,9 @@ import csv
 import os.path
 
 
-def read_file():
+def read_file(filename: str):
     data = list()
-    with open("test.csv", 'r') as f:
+    with open(filename, 'r') as f:
         reader = csv.DictReader(f, delimiter=',')
         for row in reader:
             data.append(row)
