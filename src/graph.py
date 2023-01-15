b61ec25e4ebb6f239b8a592b9575c24e3aaabcba
YoungGoofy <vbi4enkov@yandex.ru>
2023-01-15 19:40:41 +0300

    add class Graph, to drawing picture from csv

--- src/graph.py
+++ src/graph.py
@@ -1,13 +1,43 @@
 import networkx as nx
 from handlers import read_csv
+import matplotlib.pyplot as plt
 
 
-def add_nodes():
-    G = nx.Graph()
+class Graph:
+    def __init__(self):
+        self.G = nx.Graph()
+        self.add_nodes()
+        self.add_edges()
 
+    def get_headers(self):
+        headers = list()
+        filename, data = read_csv.read_file()
+        for item in data[0]:
+            headers.append(item)
+        for header in headers:
+            self.G.add_edge(filename, header)
+        return headers
 
-def get_headers():
-    headers = list()
-    for item in read_csv[0].items():
-        headers.append(item)
-    print(headers)
+    @staticmethod
+    def get_values():
+        values = list()
+        _, lst = read_csv.read_file()
+        for data in lst:
+            for value in data.values():
+                values.append(value)
+        return values
+
+    def add_nodes(self):
+        self.G.add_nodes_from(self.get_headers())
+        self.G.add_nodes_from(self.get_values())
+
+    def add_edges(self):
+        _, lst = read_csv.read_file()
+        for data in lst:
+            for item, value in data.items():
+                self.G.add_edge(item, value)
+
+    def draw_graph(self):
+        nx.draw(self.G, with_labels=True)
+        plt.savefig("test.png")
+
