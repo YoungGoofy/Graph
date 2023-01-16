92d2462a529a97dd73fbbc5072201d0f8773d455
YoungGoofy <vbi4enkov@yandex.ru>
2023-01-17 00:38:23 +0300

    fix bugs and create "add list edges" func

--- pkg/Graph/graph.go
+++ pkg/Graph/graph.go
@@ -7,12 +7,11 @@ type (
 	Nodes []Node
 	Edge  [2]Node
 	Edges []Edge
+	Graph struct {
+		Nodes []Node
+		Edges []Edge
+	}
 )
-
-type Graph struct {
-	Nodes Nodes
-	Edges Edges
-}
 
 var edge Edge
 
@@ -20,6 +19,37 @@ func (g *Graph) AddNode(n ...Node) {
 	for _, item := range n {
 		if g.checkDuplicates(item) {
 			g.Nodes = append(g.Nodes, item)
+		}
+	}
+}
+
+func (g *Graph) AddNodeList(n ...Nodes) {
+	for _, array := range n {
+		for _, item := range array {
+			if g.checkDuplicates(item) {
+				g.Nodes = append(g.Nodes, item)
+			}
+		}
+	}
+}
+
+func (g *Graph) AddEdge(node1, node2 Node) {
+	if g.findNodes(node1, node2) {
+		edge[0] = node1
+		edge[1] = node2
+		g.Edges = append(g.Edges, edge)
+	}
+}
+
+func (g *Graph) AddEdgeList(e ...Edges) {
+	for _, array := range e {
+		for _, item := range array {
+			if g.findNodes(item[0], item[1]) {
+				edge[0] = item[0]
+				edge[1] = item[1]
+				g.Edges = append(g.Edges, edge)
+			}
+
 		}
 	}
 }
@@ -32,55 +62,6 @@ func (g *Graph) DeleteNode(n Node) {
 			g.Nodes = g.Nodes[:len(g.Nodes)-1]
 		}
 	}
-}
-func (g *Graph) AddList(n ...Nodes) {
-	for _, array := range n {
-		for _, item := range array {
-			if g.checkDuplicates(item) {
-				g.Nodes = append(g.Nodes, item)
-			}
-		}
-	}
-}
-
-func (n Nodes) findNodes(node1, node2 Node) bool {
-	var count int
-	for _, item := range n {
-		if item == node1 || item == node2 {
-			count++
-		}
-	}
-	if count == 2 {
-		return true
-	}
-	return false
-}
-
-func (g Graph) CountNodes() int {
-	return len(g.Nodes)
-}
-
-func (g Graph) checkDuplicates(num Node) bool {
-	for _, item := range g.Nodes {
-		if item == num {
-			return false
-		}
-	}
-	return true
-}
-
-func (g *Graph) AddEdge(node1, node2 Node) error {
-	if g.Nodes.findNodes(node1, node2) {
-		edge[0] = node1
-		edge[1] = node2
-		g.Edges = append(g.Edges, edge)
-		return nil
-	}
-	return fmt.Errorf(`
-		Вершины не сущетвует...
-		Для проверки вершин добавьте команду fmt.Println(node.ReturnNodeList())
-		Чтоб добавить вершину добавьте команду node.AddNode() или node.AddList()
-		`)
 }
 
 func (g *Graph) DeleteEdge(e Edge) error {
@@ -95,6 +76,37 @@ func (g *Graph) DeleteEdge(e Edge) error {
 	return fmt.Errorf("Узла не существует")
 }
 
+func (g Graph) CountNodes() int {
+	return len(g.Nodes)
+}
+
 func (g Graph) CountEdges() int {
 	return len(g.Edges)
+}
+
+func (g *Graph) ClearAll() {
+	g.Nodes = []Node{}
+	g.Edges = []Edge{}
+}
+
+func (g Graph) findNodes(node1, node2 Node) bool {
+	var count int
+	for _, item := range g.Nodes {
+		if item == node1 || item == node2 {
+			count++
+		}
+	}
+	if count == 2 {
+		return true
+	}
+	return false
+}
+
+func (g Graph) checkDuplicates(num Node) bool {
+	for _, item := range g.Nodes {
+		if item == num {
+			return false
+		}
+	}
+	return true
 }
