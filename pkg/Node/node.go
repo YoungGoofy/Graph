afac5249571a1cc2d8cbc7200db01fb2ad46f3b4
YoungGoofy <vbi4enkov@yandex.ru>
2023-01-16 20:28:34 +0300

    add checker duplicates and existing elems

--- pkg/Node/node.go
+++ pkg/Node/node.go
@@ -1,7 +1,5 @@
 package node
 
-import "fmt"
-
 type Node interface{}
 type Nodes []Node
 
@@ -11,6 +9,16 @@ func AddNode(n ...Node) {
 	for _, item := range n {
 		if nodes.checkDuplicates(item) {
 			nodes = append(nodes, item)
+		}
+	}
+}
+
+func DeleteNode(n Node) {
+	for index, item := range nodes {
+		if item == n {
+			nodes[index] = nodes[len(nodes)-1]
+			nodes[len(nodes)-1] = ""
+			nodes = nodes[:len(nodes)-1]
 		}
 	}
 }
@@ -25,8 +33,8 @@ func AddList(n ...Nodes) {
 	}
 }
 
-func PrintNode() {
-	fmt.Printf("Nodes: %v", nodes)
+func ReturnNodeList() Nodes {
+	return nodes
 }
 
 func (n Nodes) checkDuplicates(num Node) bool {
@@ -36,4 +44,17 @@ func (n Nodes) checkDuplicates(num Node) bool {
 		}
 	}
 	return true
+}
+
+func (n Nodes) FindNodes(node1, node2 Node) bool {
+	var count int
+	for _, item := range n {
+		if item == node1 || item == node2 {
+			count++
+		}
+	}
+	if count == 2 {
+		return true
+	}
+	return false
 }
