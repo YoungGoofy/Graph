48b1d6d307d99fb4e7d9bad19e2cf4a5519ef94c
YoungGoofy <vbi4enkov@yandex.ru>
2023-01-16 23:57:11 +0300

    create struct with nodes and edges and delete dirs "Node" and "Edge"

---
+++ pkg/Graph/graph.go
@@ -0,0 +1,100 @@
+package graph
+
+import "fmt"
+
+type (
+	Node  interface{}
+	Nodes []Node
+	Edge  [2]Node
+	Edges []Edge
+)
+
+type Graph struct {
+	Nodes Nodes
+	Edges Edges
+}
+
+var edge Edge
+
+func (g *Graph) AddNode(n ...Node) {
+	for _, item := range n {
+		if g.checkDuplicates(item) {
+			g.Nodes = append(g.Nodes, item)
+		}
+	}
+}
+
+func (g *Graph) DeleteNode(n Node) {
+	for index, item := range g.Nodes {
+		if item == n {
+			g.Nodes[index] = g.Nodes[len(g.Nodes)-1]
+			g.Nodes[len(g.Nodes)-1] = ""
+			g.Nodes = g.Nodes[:len(g.Nodes)-1]
+		}
+	}
+}
+func (g *Graph) AddList(n ...Nodes) {
+	for _, array := range n {
+		for _, item := range array {
+			if g.checkDuplicates(item) {
+				g.Nodes = append(g.Nodes, item)
+			}
+		}
+	}
+}
+
+func (n Nodes) findNodes(node1, node2 Node) bool {
+	var count int
+	for _, item := range n {
+		if item == node1 || item == node2 {
+			count++
+		}
+	}
+	if count == 2 {
+		return true
+	}
+	return false
+}
+
+func (g Graph) CountNodes() int {
+	return len(g.Nodes)
+}
+
+func (g Graph) checkDuplicates(num Node) bool {
+	for _, item := range g.Nodes {
+		if item == num {
+			return false
+		}
+	}
+	return true
+}
+
+func (g *Graph) AddEdge(node1, node2 Node) error {
+	if g.Nodes.findNodes(node1, node2) {
+		edge[0] = node1
+		edge[1] = node2
+		g.Edges = append(g.Edges, edge)
+		return nil
+	}
+	return fmt.Errorf(`
+		Вершины не сущетвует...
+		Для проверки вершин добавьте команду fmt.Println(node.ReturnNodeList())
+		Чтоб добавить вершину добавьте команду node.AddNode() или node.AddList()
+		`)
+}
+
+func (g *Graph) DeleteEdge(e Edge) error {
+	for index, item := range g.Edges {
+		if item == e {
+			g.Edges[index] = g.Edges[len(g.Edges)-1]
+			g.Edges[len(g.Edges)-1] = Edge{}
+			g.Edges = g.Edges[:len(g.Edges)-1]
+			return nil
+		}
+	}
+	return fmt.Errorf("Узла не существует")
+}
+
+func (g Graph) CountEdges() int {
+	return len(g.Edges)
+}
